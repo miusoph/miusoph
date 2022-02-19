27636e5ed85d6c59e947d5fd1a48b6d5dde63dd9
Sophie <95385458+miusoph@users.noreply.github.com>
2022-02-18 22:42:09 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let button;
+let a = 1;
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL)
+  canvas.position(0, 0)
+  canvas.style('z-index', '-1')
+  angleMode(DEGREES)
+  button = createButton('do not click');
+  button.position(0, 0);
+  button.mousePressed(changeA);
+}
+function changeA() {
+   a = random(255);
+}
+function draw() {
+  background(30)
+  
+  rotateX(a)
+  
+  noFill()
+  stroke(255)
+  
+  for (var i= 0; i < 20 * a; i++) {
+    
+    
+    beginShape()
+    for (var j = 0; j < 360; j += 10) {
+      var rad = i * 8
+      var x = rad * cos(j)
+      var y = rad * sin(j) * a
+      var z = sin(frameCount * 2 + i * 10)  
+      
+      
+      vertex(x, y, z)
+          }
+    endShape(CLOSE)
+  }
+
+
+  
+  
+  
+
+}
+
+
