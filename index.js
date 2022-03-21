68faac88742273cc82da3d3fbc73dc05eb550d05
Sophie <95385458+miusoph@users.noreply.github.com>
2022-03-20 18:29:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -248,3 +248,17 @@ $(function () {
         loadBase62Preset(base62);
     }
 });
+
+	  ///////////////////////////////////////////////////////////////
+	 //   Generate random "Big Number" divisible by 17            //
+	///////////////////////////////////////////////////////////////
+	function randomBN17(allBits)
+	{
+		var i, j=1802, b='0';
+		if(!allBits)
+			j = Math.round(Math.random()*1802);
+		for(i=0; i<j; ++i)
+			b += new String((Math.round(Math.random() * 1234567)+ new Date().getTime()*1357)%2);
+		b17 = multBin17(b);
+		return(bin2decBN(b17));
+	}
